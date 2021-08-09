b33c9c177e0d867c7d7967fa50a82c24bc14bca1
morgan amouroux <morgan.amouroux@thoughtworks.com>
2021-08-09 12:18:15 +0700

    Setup non thread safe test (because of NUnit)

---
+++ Source/SetupLargeTest/Tests/UnitTest1.cs
@@ -0,0 +1,99 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class AClass
+    {
+        public int value { get; set; }
+
+        public AClass(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable(scope: ParallelScope.All)]
+    public class TTest
+    {
+        private AClass testable;
+
+        [SetUp]
+        public void Setup()
+        {
+            testable = new AClass(0);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(17)]
+        [TestCase(18)]
+        public void ShouldBeSameNumber0(int number)
+        {
+            testable.value = number;
+            Assert.That(testable.value, Is.EqualTo(number));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(17)]
+        [TestCase(18)]
+        public void ShouldBeSameNumber1(int number)
+        {
+            testable.value = number;
+            Assert.That(testable.value, Is.EqualTo(number));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(12)]
+        [TestCase(13)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(17)]
+        [TestCase(18)]
+        public void ShouldBeSameNumber2(int number)
+        {
+            testable.value = number;
+            Assert.That(testable.value, Is.EqualTo(number));
+        }
+    }
+}
