54472e7fe5512269db81318f75dfb91c1b398036
morgan amouroux <morgan.amouroux@thoughtworks.com>
2021-08-10 18:46:02 +0700

    Move all accounting technical setup into ModuleAccountingFake We can now imagine adding more modules in technical context and binding them with their related contexts. That would explitly tell which module depends on which context

--- Source/SetupLargeTest/Accounting.Tests/Contexts/TechnicalContext.cs
+++ Source/SetupLargeTest/Accounting.Tests/Contexts/TechnicalContext.cs
@@ -1,27 +1,19 @@
 using Accounting.Domain;
 using Accounting.Tests.Fakes;
-using Moq;
 
 namespace Accounting.Tests.Contexts
 {
     public class TechnicalContext
     {
-        private ModuleAccountingFake moduleMoney;
-        private Mock<ICurrencyConverter> currencyConverterMock;
+        private readonly ModuleAccountingFake moduleMoney;
 
         public TechnicalContext(ChangeRateContext changeRateContext)
         {
-            moduleMoney = new ModuleAccountingFake();
-            SetUpCurrencyConverter(changeRateContext);
+            moduleMoney = new ModuleAccountingFake(changeRateContext);
         }
 
-        public ICurrencyConverter CurrencyConverter => currencyConverterMock.Object;
+        public ICurrencyConverter CurrencyConverter => moduleMoney.CurrencyConverter;
 
-        private void SetUpCurrencyConverter(ChangeRateContext changeRateContext)
-        {            
-            currencyConverterMock = new Mock<ICurrencyConverter>();
-            currencyConverterMock.Setup(x => x.GetChangeRate(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns((string currencyFrom, string currencyTo) => changeRateContext.Get(currencyFrom, currencyTo));
-        }
+        
     }
 }
--- Source/SetupLargeTest/Accounting.Tests/Fakes/ModuleAccountingFake.cs
+++ Source/SetupLargeTest/Accounting.Tests/Fakes/ModuleAccountingFake.cs
@@ -1,7 +1,26 @@
+using Accounting.Domain;
+using Accounting.Tests.Contexts;
+using Moq;
+
 namespace Accounting.Tests.Fakes
 {
     public class ModuleAccountingFake
     {
+        private Mock<ICurrencyConverter> currencyConverterMock;
+
+        public ModuleAccountingFake(ChangeRateContext changeRateContext)
+        {
+            SetUpCurrencyConverter(changeRateContext);
+        }
         
+        public ICurrencyConverter CurrencyConverter => currencyConverterMock.Object;
+        
+        private void SetUpCurrencyConverter(ChangeRateContext changeRateContext)
+        {            
+            currencyConverterMock = new Mock<ICurrencyConverter>();
+            currencyConverterMock.Setup(x => x.GetChangeRate(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns((string currencyFrom, string currencyTo) => changeRateContext.Get(currencyFrom, currencyTo));
+        }
     }
+
 }
