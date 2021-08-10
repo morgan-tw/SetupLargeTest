f088d6e29ab035212d774c8b04a4747c5f28372d
morgan amouroux <morgan.amouroux@thoughtworks.com>
2021-08-10 15:57:47 +0700

    Improve CurrencyConverter setup. Make explicit that CurrencyConverter rely on ChangeRateContext.

--- Source/SetupLargeTest/Accounting.Tests/TechnicalContext.cs
+++ Source/SetupLargeTest/Accounting.Tests/TechnicalContext.cs
@@ -5,22 +5,18 @@ namespace Accounting.Tests
 {
     public class TechnicalContext
     {
-        private readonly ChangeRateContext changeRateContext;
-        
-        private readonly Mock<ICurrencyConverter> currencyConverterMock;
+        private Mock<ICurrencyConverter> currencyConverterMock;
 
         public TechnicalContext(ChangeRateContext changeRateContext)
         {
-            this.changeRateContext = changeRateContext;
-            currencyConverterMock = new Mock<ICurrencyConverter>();
-            
-            SetUpCurrencyConverter();
+            SetUpCurrencyConverter(changeRateContext);
         }
 
         public ICurrencyConverter CurrencyConverter => currencyConverterMock.Object;
 
-        private void SetUpCurrencyConverter()
-        {
+        private void SetUpCurrencyConverter(ChangeRateContext changeRateContext)
+        {            
+            currencyConverterMock = new Mock<ICurrencyConverter>();
             currencyConverterMock.Setup(x => x.GetChangeRate(It.IsAny<string>(), It.IsAny<string>()))
                 .Returns((string currencyFrom, string currencyTo) => changeRateContext.Get(currencyFrom, currencyTo));
         }
