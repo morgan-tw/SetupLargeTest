64d9299c162d041426a2667acd236af9679240cf
morgan amouroux <morgan.amouroux@thoughtworks.com>
2021-08-10 15:10:26 +0700

    Scenario already became a monster. Let's use partial to split the class by concern. This the only time I found partial useful

--- Source/SetupLargeTest/Accounting.Tests/Scenario.cs
+++ Source/SetupLargeTest/Accounting.Tests/Scenario.cs
@@ -1,15 +1,11 @@
 using Accounting.Domain;
 using Moq;
-using NUnit.Framework;
 
 namespace Accounting.Tests
 {
-    public class Scenario
+    public partial class Scenario
     {
         private readonly Mock<ICurrencyConverter> currencyConverterMock;
-
-        private readonly MoneyContext moneyContext; 
-        private Money result;
 
         private Scenario()
         {
@@ -29,89 +25,6 @@ namespace Accounting.Tests
         public Scenario But => this;
 
         #region Given
-        public Scenario AMoney(decimal amount, string currency)
-        {
-            var money = moneyContext.EnsureAMoneyExist();
-            money.Amount = amount;
-            money.Currency = currency;
-            return this;
-        }
-
-        public Scenario AMoneyHasForAmount(decimal amount)
-        {
-            moneyContext.EnsureAMoneyExist().Amount = amount;
-            return this;
-        }
-
-        public Scenario AMoneyHasForCurrency(string currency)
-        {
-            moneyContext.EnsureAMoneyExist().Currency = currency;
-            return this;
-        }
-
-        public Scenario ThisMoneyHasForCurrency(string currency)
-        {
-            return AMoneyHasForCurrency(currency);
-        }
-        
-        public Scenario ThisMoneyHasForAmount(decimal amount)
-        {
-            return AMoneyHasForAmount(amount);
-        }
-        
-        public Scenario TheMoney(string humanReadableKey, decimal amount, string currency)
-        {
-            var money = moneyContext.EnsureTheMoneyExist(humanReadableKey);
-            money.Amount = amount;
-            money.Currency = currency;
-            return this;
-        }
-        
-        public Scenario TheMoneyHasForAmount(string humanReadableKey, decimal amount)
-        {
-            var money = moneyContext.EnsureTheMoneyExist(humanReadableKey);
-            money.Amount = amount;
-            return this;
-        }
-        
-        public Scenario TheMoneyHasForCurrency(string humanReadableKey, string currency)
-        {
-            var money = moneyContext.EnsureTheMoneyExist(humanReadableKey);
-            money.Currency = currency;
-            return this;
-        }
-        #endregion
-
-        #region When
-        public Scenario WeAddTheMoneys(string leftKey, string rightKey)
-        {
-            var leftMoney = moneyContext.EnsureTheMoneyExist(leftKey);
-            var rightMoney = moneyContext.EnsureTheMoneyExist(rightKey);
-            result = leftMoney.AddUsing(rightMoney, currencyConverterMock.Object);
-            return this;
-        }
-        #endregion
-
-        #region Then
-        public Scenario ItsAmountShouldBe(decimal expectedAmount)
-        {
-            Assert.That(moneyContext.EnsureAMoneyExist().Amount, Is.EqualTo(expectedAmount));
-            return this;
-        }
-        
-        public Scenario ItsCurrencyShouldBe(string expectedCurrency)
-        {
-            Assert.That(moneyContext.EnsureAMoneyExist().Currency, Is.EqualTo(expectedCurrency));
-            return this;
-        }
-
-        public Scenario TheResultShouldBe(decimal expectedAmount, string expectedCurrency)
-        {
-            Assert.That(result.Amount, Is.EqualTo(expectedAmount));
-            Assert.That(result.Currency, Is.EqualTo(expectedCurrency));
-            return this;
-        }
-
         public Scenario TheChangeRateFromToIs(string currencyFrom, string currencyTo, decimal changeRate)
         {
             currencyConverterMock.Setup(x => x.GetChangeRate(currencyFrom, currencyTo)).Returns(changeRate);
